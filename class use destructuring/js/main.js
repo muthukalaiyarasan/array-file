f03311607c8e9b5ac39806139f0bc166ba46802c
muthukalaiyarasan <132567401+muthukalaiyarasan@users.noreply.github.com>
2024-07-23 10:42:21 +0530

    Add files via upload

---
+++ class use destructuring/js/main.js
@@ -0,0 +1,5 @@
+//destructureing
+
+ let a=[1,2,3]
+  const[name,age]=a
+    console.log(name,age)
---
+++ logic programs/js/main.js
@@ -0,0 +1,28 @@
+
+   
+  
+   function f1(){
+  let c=[2,1,4,3,5,7];
+let d=[];
+
+for(i=0;i<c.length;i++){
+	
+        if(i%2!=0){
+              d.push(i);
+		        console.log(d);
+      }
+}
+  
+ }
+ f1();
+ 
+
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+	  
